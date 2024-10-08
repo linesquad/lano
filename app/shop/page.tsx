c6c0525ce4ef909cefc570cdf7c397674328fb08
Nugo Bibilashvili <bibilashvili6@gmail.com>
2024-10-08 11:15:41 -0700

    implement grid system

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,7 +1,57 @@
+import Link from "next/link";
 import React from "react";
+import Wrapper from "../_components/Wrapper";
+import vector from "../../public/images/vector.svg";
+import Image from "next/image";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="p-5">
+      <Wrapper>
+        <div className="flex gap-1 text-xs text-[#00000099] px-5 pt-6 pb-4">
+          <Link href="/">მთავარი</Link>
+          <span className=""> &gt; </span>
+          <span className="">Shop</span>
+        </div>
+        <div className="grid grid-cols-[1fr_4fr] gap-5">
+          <div className="w-full p-5">
+            <ul className="text-[#000000] text-base font-medium flex flex-col gap-5">
+              <li className="flex items-center justify-between">
+                <span>ძაღლები</span>
+                <Image
+                  src={vector}
+                  alt="vector svg"
+                  className="cursor-pointer"
+                />
+              </li>
+              <li className="flex items-center justify-between">
+                <span>კატები</span>
+                <Image
+                  src={vector}
+                  alt="vector svg"
+                  className="cursor-pointer"
+                />
+              </li>
+              <li className="flex items-center justify-between">
+                <span>ჩიტები</span>
+                <Image
+                  src={vector}
+                  alt="vector svg"
+                  className="cursor-pointer"
+                />
+              </li>
+            </ul>
+          </div>
+          <div className="grid grid-cols-4 gap-6">
+            <div>nugo</div>
+            <div>nugo</div>
+            <div>nugo</div>
+            <div>nugo</div>
+          </div>
+        </div>
+      </Wrapper>
+    </div>
+  );
 };
 
 export default page;
