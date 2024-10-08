9bf08c5a4df7a94e370b7aa053f134704a8ab7a8
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-10-08 18:11:13 +0400

    feat: add popular product

--- app/_components/Home.tsx
+++ app/_components/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
 import SaleProducts from "./SaleProducts";
+import PopularProducts from "./PopularProducts";
 
 const Home = (): JSX.Element => {
   return (
     <div>
       <Hero />
       <SaleProducts />
+      <PopularProducts />
     </div>
   );
 };
---
+++ app/_components/PopularProducts.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Wrapper from "./Wrapper";
+import SingleProduct from "./SingleProduct";
+import { useEffect, useState } from "react";
+
+export default function PopularProducts() {
+  const [itemsPerPage, setItemsPerPage] = useState(0);
+
+  const handleResize = () => {
+    const width = window.innerWidth;
+    if (width >= 1024) {
+      setItemsPerPage(6);
+    } else if (width >= 768) {
+      setItemsPerPage(4);
+    } else {
+      setItemsPerPage(2);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <div>
+      <Wrapper>
+        <div>
+          <h2 className="text-[16px] text-[#000000] font-medium mt-[32px] ml-[20px] ">
+            პოპულარული პროდუქტები
+          </h2>
+          <div className="ml-[20px] mt-[16px]">
+            <Carousel className="w-full">
+              <CarouselContent>
+                {Array.from({ length: 5 }).map((_, index) => (
+                  <CarouselItem
+                    key={index}
+                    className="flex justify-center items-center mt-auto"
+                  >
+                    {Array.from({ length: itemsPerPage }).map((_, i) => (
+                      <div key={i}>
+                        <SingleProduct />
+                      </div>
+                    ))}
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </Carousel>
+          </div>
+        </div>
+      </Wrapper>
+    </div>
+  );
+}
