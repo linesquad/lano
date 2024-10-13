1be21c8a24337d268e341795863ffd5941661536
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-10-13 16:30:13 +0400

    clean up shadcn and implement swiper

--- app/_components/TrendCarousel.tsx
+++ app/_components/TrendCarousel.tsx
@@ -1,43 +1,14 @@
 "use client";
 
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
 import Wrapper from "./Wrapper";
-import SingleProduct from "./SingleProduct";
-import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 interface TrendCarouselProps {
   title: string;
 }
 
 export default function TrendCarousel({ title }: TrendCarouselProps) {
-  const [itemsPerPage, setItemsPerPage] = useState(0);
-
-  const handleResize = () => {
-    const width = window.innerWidth;
-    if (width >= 1024) {
-      setItemsPerPage(5);
-    } else if (width >= 768) {
-      setItemsPerPage(4);
-    } else if (width >= 450) {
-      setItemsPerPage(3);
-    } else {
-      setItemsPerPage(2);
-    }
-  };
-
-  useEffect(() => {
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
   return (
     <div>
       <Wrapper>
@@ -46,24 +17,11 @@ export default function TrendCarousel({ title }: TrendCarouselProps) {
             {title}
           </h2>
           <div className="ml-[20px] mt-[16px]">
-            <Carousel className="w-full">
-              <CarouselContent>
-                {Array.from({ length: 5 }).map((_, index) => (
-                  <CarouselItem
-                    key={index}
-                    className="flex justify-center items-center mt-auto"
-                  >
-                    {Array.from({ length: itemsPerPage }).map((_, i) => (
-                      <div key={i}>
-                        <SingleProduct />
-                      </div>
-                    ))}
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-              <CarouselPrevious className="md:ml-[20px] lg:ml-[40px] xl:ml-[50px] hidden md:flex items-center justify-center" />
-              <CarouselNext className="md:mr-[20px] lg:mr-[40px] xl:mr-[50px] hidden md:flex items-center justify-center" />
-            </Carousel>
+            <Swiper spaceBetween={50} slidesPerView={1}>
+              <SwiperSlide>Slide 1</SwiperSlide>
+              <SwiperSlide>Slide 2</SwiperSlide>
+              <SwiperSlide>Slide 3</SwiperSlide>
+            </Swiper>
           </div>
         </div>
       </Wrapper>
