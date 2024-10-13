6b30a272ed9aa2b7b7087d144d716bfe69e291e1
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-10-13 22:41:37 +0400

    work in progress with singleProduct prices

--- app/_components/PopularProducts.tsx
+++ app/_components/PopularProducts.tsx
@@ -4,7 +4,7 @@ import TrendCarousel from "./TrendCarousel";
 
 export default async function PopularProducts() {
   const popularProducts: ILandingProducts[] = await fetchPopularProducts();
-
+  console.log(popularProducts);
   return (
     <div>
       <TrendCarousel title="პოპულარული პროდუქტები" products={popularProducts} />
--- app/_components/SingleProduct.tsx
+++ app/_components/SingleProduct.tsx
@@ -9,14 +9,15 @@ export default function SingleProduct({ product }: SingleProductProps) {
   return (
     <div>
       <div className="pt-[8px] pl-[8px] relative min-w-[150px] w-[150px] md:min-w-[170px] md:w-[170px] lg:min-w-[200px] lg:w-[200px]">
-        <Image
-          src="/images/product.png"
-          alt="პროდუცტ"
-          width={150}
-          height={150}
-          priority
-          className="rounded-[7px]"
-        />
+        <div className="w-[150px] h-[150px] object-cover rounded-[7px]">
+          <Image
+            src={product.image}
+            alt="პროდუცტ"
+            priority
+            fill
+            className="object-cover rounded-[7px]"
+          />
+        </div>
         {product.discount !== 0 && (
           <div className="w-[60px] h-[32px] bg-[#EE5335] flex justify-center items-center rounded-[4px] absolute top-[8px] left-[8px]">
             <span className="text-[#fff] font-semibold">SALE</span>
@@ -25,11 +26,11 @@ export default function SingleProduct({ product }: SingleProductProps) {
       </div>
       <div className="flex items-center gap-[8px] mt-[8px]">
         <p className="text-[16px] text-[#FF3B30] font-bold">
-          {product.price.$numberDecimal} ₾
+          {product.price.$numberDecimal}₾
         </p>
         {product.discount !== 0 && (
           <p className="text-[14px] text-[#00000066] font-medium line-through">
-            {product.discount}₾
+            {product.price.$numberDecimal}₾
           </p>
         )}
       </div>
