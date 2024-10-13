d438344db908f010844246dbfedde2252c421d7c
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-10-13 17:16:47 +0400

    implement swiper variant reusable component functionality

--- app/_components/TrendCarousel.tsx
+++ app/_components/TrendCarousel.tsx
@@ -2,13 +2,31 @@
 
 import Wrapper from "./Wrapper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useRef } from "react";
 import "swiper/css";
+import SingleProduct from "./SingleProduct";
+import Image from "next/image";
+import { Swiper as SwiperCore } from "swiper";
 
 interface TrendCarouselProps {
   title: string;
 }
 
 export default function TrendCarousel({ title }: TrendCarouselProps) {
+  const swiperRef = useRef<SwiperCore>();
+
+  const handlePrev = () => {
+    if (swiperRef.current) {
+      swiperRef.current.slidePrev();
+    }
+  };
+
+  const handleNext = () => {
+    if (swiperRef.current) {
+      swiperRef.current.slideNext();
+    }
+  };
+
   return (
     <div>
       <Wrapper>
@@ -16,12 +34,79 @@ export default function TrendCarousel({ title }: TrendCarouselProps) {
           <h2 className="text-[16px] text-[#000000] font-medium mt-[32px] ml-[20px] ">
             {title}
           </h2>
-          <div className="ml-[20px] mt-[16px]">
-            <Swiper spaceBetween={50} slidesPerView={1}>
-              <SwiperSlide>Slide 1</SwiperSlide>
-              <SwiperSlide>Slide 2</SwiperSlide>
-              <SwiperSlide>Slide 3</SwiperSlide>
+          <div className="ml-[20px] mt-[16px] flex items-center gap-[3px]">
+            <div
+              onClick={handlePrev}
+              className="hidden w-[84px] h-[44px] shadow-lg shadow-[#00000026] rounded-[50%] md:flex items-center justify-center cursor-pointer"
+            >
+              <Image
+                src="/images/carouselArrow.svg"
+                alt="arrow"
+                width={14}
+                height={10}
+                className="rotate-180"
+              />
+            </div>
+
+            <Swiper
+              onSwiper={(swiper) => {
+                swiperRef.current = swiper;
+              }}
+              slidesPerView={2}
+              breakpoints={{
+                450: {
+                  slidesPerView: 3,
+                },
+                740: {
+                  slidesPerView: 4,
+                },
+                1024: {
+                  slidesPerView: 5,
+                },
+              }}
+            >
+              <SwiperSlide>
+                <SingleProduct />
+              </SwiperSlide>
+              <SwiperSlide>
+                <SingleProduct />
+              </SwiperSlide>
+              <SwiperSlide>
+                <SingleProduct />
+              </SwiperSlide>
+              <SwiperSlide>
+                <SingleProduct />
+              </SwiperSlide>
+              <SwiperSlide>
+                <SingleProduct />
+              </SwiperSlide>
+              <SwiperSlide>
+                <SingleProduct />
+              </SwiperSlide>
+              <SwiperSlide>
+                <SingleProduct />
+              </SwiperSlide>
+              <SwiperSlide>
+                <SingleProduct />
+              </SwiperSlide>
+              <SwiperSlide>
+                <SingleProduct />
+              </SwiperSlide>
+              <SwiperSlide>
+                <SingleProduct />
+              </SwiperSlide>
             </Swiper>
+            <div
+              onClick={handleNext}
+              className="hidden w-[84px] h-[44px] shadow-lg shadow-[#00000026] rounded-[50%] md:flex items-center justify-center cursor-pointer"
+            >
+              <Image
+                src="/images/carouselArrow.svg"
+                alt="arrow"
+                width={14}
+                height={10}
+              />
+            </div>
           </div>
         </div>
       </Wrapper>
