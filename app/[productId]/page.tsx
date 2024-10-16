da0380a30c7bf8682912b09028daa3b05540271c
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-10-16 22:36:11 +0400

    wip: productId

---
+++ app/[productId]/page.tsx
@@ -0,0 +1,25 @@
+import ProductHeader from "./_components/ProductHeader";
+import ProductDetails from "./_components/ProductDetails";
+import ProductDescription from "./_components/ProductDescription";
+import { fetchOneProduct } from "@/app/api";
+import { IOneProduct } from "@/types/types";
+
+interface PageProps {
+  params: {
+    productId: string;
+  };
+}
+
+const page = async ({ params }: PageProps) => {
+  const oneProduct: IOneProduct = await fetchOneProduct(params.productId);
+
+  return (
+    <div>
+      <ProductHeader />
+      <ProductDetails oneProduct={oneProduct} />
+      <ProductDescription oneProduct={oneProduct} />
+    </div>
+  );
+};
+
+export default page;
