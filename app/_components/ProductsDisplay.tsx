cede368fe14fac30d6beecd0bbbc515ae0f5d227
marriamdd <davitashvili.m3@gmail.com>
2024-10-16 14:38:54 +0400

    add product display

---
+++ app/_components/ProductsDisplay.tsx
@@ -0,0 +1,21 @@
+import { fetchPopularProducts, fetchSaleProducts } from "../api";
+import TrendCarousel from "./TrendCarousel";
+
+async function fetchProducts() {
+  const [saleProducts, popularProducts] = await Promise.all([
+    fetchSaleProducts(),
+    fetchPopularProducts(),
+  ]);
+  return { saleProducts, popularProducts };
+}
+
+export default async function ProductsDisplay() {
+  const { saleProducts, popularProducts } = await fetchProducts();
+
+  return (
+    <div>
+      <TrendCarousel title="ფასდაკლებული პროდუქტები" products={saleProducts} />
+      <TrendCarousel title="პოპულარული პროდუქტები" products={popularProducts} />
+    </div>
+  );
+}
