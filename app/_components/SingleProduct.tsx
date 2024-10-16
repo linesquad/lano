98ff97d021db7d8db9c51552350a579d8d29e3eb
tornike2002 <toko.toka2000@gmail.com>
2024-10-17 01:42:11 +0400

    fix singleProduct

--- app/_components/SingleProduct.tsx
+++ app/_components/SingleProduct.tsx
@@ -9,44 +9,24 @@ interface SingleProductProps {
 
 export default function SingleProduct({ product }: SingleProductProps) {
   console.log(product, "product");
-
   return (
-    <Link href={`/${product.title}/${product._id}`}>
-      <div className="pt-[8px] pl-[8px] relative min-w-[150px] w-[150px] md:min-w-[170px] md:w-[170px] lg:min-w-[200px] lg:w-[200px]">
-        <div className="w-[150px] h-[150px] object-cover rounded-[7px]">
-          <Image
-            src={product.image}
-            alt="პროდუცტ"
-            priority
-            fill
-            className="object-cover group-hover:hidden rounded-[7px] transition duration-300"
-          />
-          {product.discount !== 0 && (
-            <div className="w-[60px] h-[32px] bg-[#EE5335] group-hover:hidden flex justify-center items-center rounded-[4px] absolute top-[8px] left-[8px]">
-              <span className="text-[#fff] font-semibold">SALE</span>
-            </div>
-          )}
-          <div className="p-[10px]">
-            <div className="  items-center gap-[8px] group-hover:flex hidden transition duration-300">
-              <p className="text-[16px] text-[#FF3B30] font-bold">
-                {product.currentPrice}₾ {/* Use $numberDecimal */}
-              </p>
-              {product.discount !== 0 && (
-                <p className="text-[14px] text-[#00000066] font-medium line-through">
-                  {product.price.$numberDecimal}₾ {/* Use $numberDecimal */}
-                </p>
-              )}
-            </div>
-            <p className="text-[14px] text-[#000000] font-medium mt-[8px] group-hover:block transition duration-300">
-              {product.title.length > 10
-                ? product.title.slice(0, 10) + "..."
-                : product.title}
-            </p>
+    <div className="flex flex-col items-center h-[230px]">
+    <div className="group relative min-w-[150px] w-[150px] md:min-w-[170px] md:w-[170px] lg:min-w-[200px] lg:w-[200px] transition duration-300">
+      <div className="w-full h-[150px] relative rounded-[7px] overflow-hidden border border-transparent  hover:border-black group-hover:border group-hover:border-black">
+        <Image
+          src={product.image}
+          alt="პროდუცტ"
+          priority
+          fill
+          className="object-cover group-hover:hidden rounded-[7px] transition duration-300"
+        />
+        {product.discount !== 0 && (
+          <div className="w-[60px] h-[32px] bg-[#EE5335] group-hover:hidden flex justify-center items-center rounded-[4px] absolute top-[8px] left-[8px]">
+            <span className="text-[#fff] font-semibold">SALE</span>
           </div>
-        </div>
-
-        <div className="mt-[8px] p-[8px]">
-          <div className="flex items-center gap-[8px] group-hover:hidden transition duration-300">
+        )}
+        <div className="p-[10px]">
+          <div className="  items-center gap-[8px] group-hover:flex hidden transition duration-300">
             <p className="text-[16px] text-[#FF3B30] font-bold">
               {product.currentPrice}₾ {/* Use $numberDecimal */}
             </p>
@@ -56,26 +36,40 @@ export default function SingleProduct({ product }: SingleProductProps) {
               </p>
             )}
           </div>
-          <p className="text-[14px] text-[#000000] font-medium mt-[8px] group-hover:hidden transition duration-300">
+          <p className="text-[14px] text-[#000000] font-medium mt-[8px] group-hover:block transition duration-300">
             {product.title.length > 10
               ? product.title.slice(0, 10) + "..."
               : product.title}
           </p>
-
-          <Link href={`/shop/${product._id}`}>
-            <div className="hidden group-hover:block mt-[8px] p-[8px] transition duration-300">
-              <button className="w-full  text-black py-2 rounded-[7px] border border-black transition duration-300">
-                ვრცლად
-              </button>
-            </div>
-          </Link>
         </div>
       </div>
-      <p className="text-[14px] text-[#000000] font-medium mt-[8px]">
-        {product.title.length > 10
-          ? product.title.slice(0, 10) + "..."
-          : product.title}
-      </p>
-    </Link>
+
+      <div className="mt-[8px] p-[8px]">
+        <div className="flex items-center gap-[8px] group-hover:hidden transition duration-300">
+          <p className="text-[16px] text-[#FF3B30] font-bold">
+            {product.currentPrice}₾ {/* Use $numberDecimal */}
+          </p>
+          {product.discount !== 0 && (
+            <p className="text-[14px] text-[#00000066] font-medium line-through">
+              {product.price.$numberDecimal}₾ {/* Use $numberDecimal */}
+            </p>
+          )}
+        </div>
+        <p className="text-[14px] text-[#000000] font-medium mt-[8px] group-hover:hidden transition duration-300">
+          {product.title.length > 10
+            ? product.title.slice(0, 10) + "..."
+            : product.title}
+        </p>
+
+        <Link href={`/shop/${product._id}`}>
+          <div className="hidden group-hover:block mt-[8px] p-[8px] transition duration-300">
+            <button className="w-full  text-black py-2 rounded-[7px] border border-black transition duration-300">
+              ვრცლად
+            </button>
+          </div>
+        </Link>
+      </div>
+    </div>
+  </div>
   );
 }
