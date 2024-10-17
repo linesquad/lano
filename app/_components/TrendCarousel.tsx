07d7f1e91859bf55f54ac45a5524fec778e5361c
tornike2002 <toko.toka2000@gmail.com>
2024-10-17 16:00:19 +0400

    handled arrow show hide function in trendCarousel

--- app/_components/TrendCarousel.tsx
+++ app/_components/TrendCarousel.tsx
@@ -2,7 +2,7 @@
 
 import Wrapper from "./Wrapper";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "swiper/css";
 import Image from "next/image";
 import { Swiper as SwiperCore } from "swiper";
@@ -16,6 +16,8 @@ interface TrendCarouselProps {
 
 export default function TrendCarousel({ title, products }: TrendCarouselProps) {
   const swiperRef = useRef<SwiperCore | null>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [canScrollRight, setCanScrollRight] = useState(true);
 
   const handlePrev = () => {
     swiperRef.current?.slidePrev();
@@ -27,7 +29,8 @@ export default function TrendCarousel({ title, products }: TrendCarouselProps) {
 
   const productArray = products.products;
 
-  console.log("Products:", productArray);
+  const showRightArrow =
+    currentIndex < productArray.length - Math.min(productArray.length, 5);
 
   return (
     <div>
@@ -37,22 +40,30 @@ export default function TrendCarousel({ title, products }: TrendCarouselProps) {
             {title}
           </h2>
           <div className="ml-[20px] mt-[16px] flex items-center gap-[3px]">
-            <div
-              onClick={handlePrev}
-              className="hidden w-[44px] h-[44px] shadow-lg shadow-[#00000026] rounded-[50%] md:flex items-center justify-center cursor-pointer"
-            >
-              <Image
-                src="/images/carouselArrow.svg"
-                alt="arrow"
-                width={14}
-                height={10}
-                className="rotate-180"
-              />
+            <div className="relative">
+              {currentIndex > 0 && (
+                <div
+                  onClick={handlePrev}
+                  className="absolute bottom-0 left-0 w-[44px] h-[44px] bg-white shadow-lg
+                    shadow-[#00000026] rounded-full md:flex items-center justify-center cursor-pointer
+                    z-50 hover:bg-red-400 transition-all duration-500 ease-in-out"
+                >
+                  <Image
+                    src="/images/carouselArrow.svg"
+                    alt="arrow"
+                    width={14}
+                    height={10}
+                    className="rotate-180"
+                  />
+                </div>
+              )}
             </div>
-
             <Swiper
               onSwiper={(swiper) => {
                 swiperRef.current = swiper;
+              }}
+              onSlideChange={(swiper) => {
+                setCurrentIndex(swiper.activeIndex);
               }}
               slidesPerView={Math.min(productArray.length, 5)}
               breakpoints={{
@@ -77,16 +88,22 @@ export default function TrendCarousel({ title, products }: TrendCarouselProps) {
               ))}
             </Swiper>
 
-            <div
-              onClick={handleNext}
-              className="hidden w-[44px] h-[44px] shadow-lg shadow-[#00000026] rounded-[50%] md:flex items-center justify-center cursor-pointer"
-            >
-              <Image
-                src="/images/carouselArrow.svg"
-                alt="arrow"
-                width={14}
-                height={10}
-              />
+            <div className="relative">
+              {showRightArrow && (
+                <div
+                  onClick={handleNext}
+                  className="absolute bottom-0 right-0 w-[44px] h-[44px] bg-white shadow-lg
+                    shadow-[#00000026] rounded-full md:flex items-center justify-center cursor-pointer
+                    z-50 hover:bg-red-400 transition-all duration-500 ease-in-out"
+                >
+                  <Image
+                    src="/images/carouselArrow.svg"
+                    alt="arrow"
+                    width={14}
+                    height={10}
+                  />
+                </div>
+              )}
             </div>
           </div>
         </div>
