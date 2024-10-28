5d1fcb94bca062134729fe118564967023dd7aab
Nugo Bibilashvili <bibilashvili6@gmail.com>
2024-10-28 02:17:35 -0700

    change cache

--- app/api/index.ts
+++ app/api/index.ts
@@ -4,7 +4,7 @@ const URL = "https://lano2024-0b1bbc3f481c.herokuapp.com/";
 export const fetchPopularProducts = async () => {
   try {
     const response = await fetch(`${URL}product?popular=true&page=1`, {
-      cache: "default",
+      cache: "no-cache",
     });
     if (!response.ok) throw new Error(`Error: ${response.status}`);
 
@@ -18,7 +18,7 @@ export const fetchPopularProducts = async () => {
 export const fetchProduct = async (page: number): Promise<Product | null> => {
   try {
     const response = await fetch(`${URL}product?products=true&page=${page}`, {
-      cache: "default",
+      cache: "no-cache",
     });
     if (!response.ok) throw new Error(`Error: ${response.status}`);
     const data: Product = await response.json();
@@ -32,7 +32,7 @@ export const fetchProduct = async (page: number): Promise<Product | null> => {
 export const fetchCategory = async (): Promise<Category[]> => {
   try {
     const response = await fetch(`${URL}category`, {
-      cache: "default",
+      cache: "no-cache",
     });
     if (!response.ok) throw new Error(`Error: ${response.status}`);
 
@@ -47,7 +47,7 @@ export const fetchCategory = async (): Promise<Category[]> => {
 export const fetchSaleProducts = async () => {
   try {
     const response = await fetch(`${URL}product?discount=true&page=1`, {
-      cache: "default",
+      cache: "no-cache",
     });
     if (!response.ok) throw new Error(`Error: ${response.status}`);
 
@@ -65,7 +65,7 @@ export const fetchByCatId = async (
 ): Promise<Product | null> => {
   try {
     const response = await fetch(`${URL}product?catId=${id}&page=${page}`, {
-      cache: "default",
+      cache: "no-cache",
     });
     if (!response.ok) throw new Error(`Error: ${response.status}`);
     const dataById: Product = await response.json();
@@ -79,7 +79,7 @@ export const fetchByCatId = async (
 export const fetchOneProduct = async (id: string) => {
   try {
     const response = await fetch(`${URL}product?productId=${id}`, {
-      cache: "default",
+      cache: "no-cache",
     });
     if (!response.ok) throw new Error(`Error: ${response.status}`);
 
