c4505660035caefc3ebed2aab9b8f5ebbfae5b2a
Nugo Bibilashvili <bibilashvili6@gmail.com>
2024-11-25 06:21:06 -0800

    fix product display after filter

--- app/[locale]/shop/[...SubCatId]/page.tsx
+++ app/[locale]/shop/[...SubCatId]/page.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import { fetchByCatId } from "../../../api/index";
 import SingleItemDisplay from "../_components/SingleItemDisplay";
 
-const Page = async ({ params }: { params: { SubCatId: string[] } }) => {
+const Page = async ({
+  params,
+}: {
+  params: { SubCatId: string[]; locale: string };
+}) => {
+  const locale = params.locale;
   const id = params.SubCatId[2];
   const response = await fetchByCatId(id, 1);
   if (!response || !response.products || response.products.length === 0) {
@@ -18,7 +23,7 @@ const Page = async ({ params }: { params: { SubCatId: string[] } }) => {
       <div className="grid grid-cols-3 gap-6 tiny:grid-cols-2 smaller:grid-cols-2 md:grid-cols-4">
         {products.map((item) => (
           <div key={item._id} className="tiny:max-w-[157px] tiny:max-h-[233px]">
-            <SingleItemDisplay item={item} />
+            <SingleItemDisplay item={item} locale={locale} />
           </div>
         ))}
       </div>
